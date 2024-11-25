e687a39b6b2162650fedfc4d956fd567d7e41a74
Alejandro Parra <alejandro.parra@iepcdurango.mx>
2024-11-25 15:04:53 -0600

    Ajustes formulario

--- src/components/Registration/Form.vue
+++ src/components/Registration/Form.vue
@@ -42,8 +42,8 @@ import axios from "axios";
 
 const voterCardSchema = z.object({
   cic: z.string({required_error: 'Requerido'}).min(9).max(9).optional(),
-  ocr: z.string({required_error: 'Requerido'}).min(1).max(999).optional(),
-  section: z.string({required_error: 'Requerido'}).min(1).max(9999).optional(),
+  ocr: z.string({required_error: 'Requerido'}).min(13).max(13).optional(),
+  section: z.string({required_error: 'Requerido'}).min(1).max(4).optional(),
   emission_number: z.string({required_error: 'Requerido'}).min(1).max(99).optional(),
 });
 
