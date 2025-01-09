f0031ce39bb9f7d72f09994fcc6e885b62be1250
Alejandro Parra <alejandro.parra@iepcdurango.mx>
2025-01-09 01:27:44 -0600

    Cambios mínimos formulario.

--- src/components/Registration/Form/Modules/GeneralInformation.vue
+++ src/components/Registration/Form/Modules/GeneralInformation.vue
@@ -8,28 +8,30 @@ import {vMaska} from "maska/vue"
 </script>
 
 <template>
-  <div class="grid grid-cols-3 gap-4">
-    <div>
-      <FormField v-slot="{ componentField }" name="first_name">
-        <FormItem>
-          <FormLabel>Primer apellido</FormLabel>
-          <FormControl>
-            <Input type="text" placeholder="Primer apellido" v-bind="componentField"/>
-          </FormControl>
-          <FormMessage/>
-        </FormItem>
-      </FormField>
-    </div>
-    <div>
-      <FormField v-slot="{ componentField }" name="second_name">
-        <FormItem>
-          <FormLabel>Segundo apellido</FormLabel>
-          <FormControl>
-            <Input type="text" placeholder="Segundo apellido" v-bind="componentField"/>
-          </FormControl>
-          <FormMessage/>
-        </FormItem>
-      </FormField>
+  <div class="grid sm:grid-cols-1 xl:grid-cols-2 gap-4 mt-5">
+    <div class="grid grid-cols-2 gap-4">
+      <div>
+        <FormField v-slot="{ componentField }" name="first_name">
+          <FormItem>
+            <FormLabel>Primer apellido</FormLabel>
+            <FormControl>
+              <Input type="text" placeholder="Primer apellido" v-bind="componentField"/>
+            </FormControl>
+            <FormMessage/>
+          </FormItem>
+        </FormField>
+      </div>
+      <div>
+        <FormField v-slot="{ componentField }" name="second_name">
+          <FormItem>
+            <FormLabel>Segundo apellido</FormLabel>
+            <FormControl>
+              <Input type="text" placeholder="Segundo apellido" v-bind="componentField"/>
+            </FormControl>
+            <FormMessage/>
+          </FormItem>
+        </FormField>
+      </div>
     </div>
     <div>
       <FormField v-slot="{ componentField }" name="name">
@@ -43,7 +45,7 @@ import {vMaska} from "maska/vue"
       </FormField>
     </div>
   </div>
-  <div class="grid grid-cols-3 gap-4">
+  <div class="grid grid-cols-1 xl:grid-cols-2 gap-4">
     <div>
       <FormField v-slot="{ componentField }" name="occupation">
         <FormItem>
